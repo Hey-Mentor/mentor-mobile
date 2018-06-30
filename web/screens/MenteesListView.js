7388c2e06d5c33d1d5132e5a836117dfb74b183f
Glen Pham <phamgalang@gmail.com>
2018-06-30 16:36:17 -0700

    changed login logic to persist, removed login header (#7)

--- web/screens/MenteesListView.js
+++ web/screens/MenteesListView.js
@@ -1,43 +1,75 @@
 import React, { Component } from 'react';
 import { Icon } from 'react-native-elements';
 import MenteeList from '../components/menteeList/MenteeList';
-import { ScrollView, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  AsyncStorage
+} from 'react-native';
 
 class MenteeListView extends Component {
+  state = {
+    menteeItem: [],
+    fbToken: '',
+    fbUserId: ''
+  };
 
-  constructMenteeItemsFromResponse = async (menteeIds) => {
+  async componentDidMount() {
+    const token = await AsyncStorage.getItem('fb_token');
+    const id = await AsyncStorage.getItem('fb_id');
+
+    this.setState({
+      fbToken: token,
+      fbUserId: id
+    });
+    this.getUserData(this.state.fbUserId);
+  }
+
+  constructMenteeItemsFromResponse = async menteeIds => {
     menteeItems = [];
 
-    for (let mentee of menteeIds)
-    {
-      let response = await fetch(`https://heymentortestdeployment.herokuapp.com/mentees/${mentee}`);
+    for (let mentee of menteeIds) {
+      let response = await fetch(
+        `https://heymentortestdeployment.herokuapp.com/mentees/${mentee}`
+      );
       let responseJson = await response.json();
 
       console.log(responseJson);
 
-      fullName = responseJson[0].person.fname + " " + responseJson[0].person.lname;
-      menteeItems.push({"name": fullName, "school": responseJson[0].school.name, "grade": responseJson[0].school.grade, "id": responseJson[0].mentee_id, "fullMentee": responseJson[0] });
+      fullName =
+        responseJson[0].person.fname + ' ' + responseJson[0].person.lname;
+      menteeItems.push({
+        name: fullName,
+        school: responseJson[0].school.name,
+        grade: responseJson[0].school.grade,
+        id: responseJson[0].mentee_id,
+        fullMentee: responseJson[0]
+      });
     }
 
     this.setState({ menteeItem: menteeItems });
 
-    console.log("mentee items");  
+    console.log('mentee items');
     console.log(menteeItems);
-  }
+  };
 
-  getUserData = async (userId) => {
-    console.log("FacebookID: " + userId );
+  getUserData = async userId => {
+    console.log('FacebookID: ' + userId);
 
-    let response = await fetch(`https://heymentortestdeployment.herokuapp.com/mentors/${userId}`);
+    let response = await fetch(
+      `https://heymentortestdeployment.herokuapp.com/mentors/${userId}`
+    );
     let responseJson = await response.json();
-  
-    console.log("Printing responsejson");
+
+    console.log('Printing responsejson');
     console.log(responseJson);
 
-    console.log("Mentees:");
-    console.log(responseJson[0].mentee_ids)
+    console.log('Mentees:');
+    console.log(responseJson[0].mentee_ids);
     this.constructMenteeItemsFromResponse(responseJson[0].mentee_ids);
-  }
+  };
 
   static navigationOptions = ({ navigation }) => ({
     title: 'Mentees',
@@ -48,27 +80,32 @@ class MenteeListView extends Component {
           navigation.navigate('notifications');
         }}
       >
-        <Icon name="ios-notifications" type="ionicon" size={40} iconStyle={styles.leftImage} />
+        <Icon
+          name="ios-notifications"
+          type="ionicon"
+          size={40}
+          iconStyle={styles.leftImage}
+        />
       </TouchableOpacity>
     ),
 
-    headerRight: <Icon name="gear" type="font-awesome" size={40} iconStyle={styles.rightImage} />
+    headerRight: (
+      <Icon
+        name="gear"
+        type="font-awesome"
+        size={40}
+        iconStyle={styles.rightImage}
+      />
+    )
   });
 
-  state = {
-    menteeItem: []
-  };  
-
   render() {
-    const { state, navigate } = this.props.navigation;
-
-    if (this.state.menteeItem.length <= 0){
-      this.getUserData(state.params.fbId);  
-    }
-
     return (
       <ScrollView>
-        <MenteeList menteeItem={this.state.menteeItem} navigation={this.props.navigation} />
+        <MenteeList
+          menteeItem={this.state.menteeItem}
+          navigation={this.props.navigation}
+        />
       </ScrollView>
     );
   }
