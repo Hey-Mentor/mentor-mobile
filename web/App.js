f5e6aada1f88ddcf7cf63190770ef28758bbee5c
Matthew Bongiovi <mattbo@microsoft.com>
2018-11-15 14:08:55 -0800

    Begin merging chat screen

--- web/App.js
+++ web/App.js
@@ -8,15 +8,24 @@ import MenteeDetailsView from './screens/MenteeDetailsView';
 import MenteesListView from './screens/MenteesListView';
 import Settings from './screens/Settings';
 import registerForNotifications from './services/pushNotifications';
+import ChatScreen from './screens/ChatScreen'
 
 export default class App extends React.Component {
   render() {
+    const userName = 'Matt';
     const MainNavigator = StackNavigator({
       home: { screen: HomeAuth },
       notifications: { screen: NotificationsScreen },
       menteeListView: { screen: MenteesListView },
       menteeDetails: { screen: MenteeDetailsView },
-      settings: { screen: Settings }
+      settings: { screen: Settings },
+      chat: {
+        screen: ChatScreen,
+        navigationOptions: ({ navigation }) => ({
+          title: `💬 ${userName}`, // TODO: Specify recipient name
+          headerTitleStyle: headerTitleStyle
+        })
+      },
     });
     return <MainNavigator />;
   }
