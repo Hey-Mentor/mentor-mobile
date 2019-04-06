88314e64ab4a1262a5323ab14163bd82c7254f8e
Matt Bongiovi <bongiovimatthew@gmail.com>
2019-04-06 11:51:02 -0700

    Fix config style

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // CONFIG file hosting URI variables
 
 // API URL
-let API_URL = 'http://10.104.148.19:8081';
+const API_URL = 'http://10.91.28.70:8081';
 // let API_URI = 'http://ppeheymentor-env.qhsppj9piv.us-east-2.elasticbeanstalk.com';
-export {API_URL};
+export { API_URL };
