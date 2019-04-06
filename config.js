cdd2dfda86d88dbdef4d27a4f1421830e168e713
Aaron Yim <aaronyim@outlook.com>
2019-04-05 21:00:59 -0700

    Removed un-needed react import from config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+// CONFIG file hosting URI variables
 
-// URL FOR API 
-
+// API URL
 let API_URL = 'http://10.104.148.19:8081';
 // let API_URI = 'http://ppeheymentor-env.qhsppj9piv.us-east-2.elasticbeanstalk.com';
 export {API_URL};
