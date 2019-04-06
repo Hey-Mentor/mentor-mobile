1993ebbe1490942f70bb0090eb7b978017f8ea14
Aaron Yim <aaronyim@outlook.com>
2019-04-05 20:39:33 -0700

    Added config.js to centralize URI variable storage

---
+++ config.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+// URL FOR API 
+
+let API_URL = 'http://10.104.148.19:8081';
+// let API_URI = 'http://ppeheymentor-env.qhsppj9piv.us-east-2.elasticbeanstalk.com';
+export {API_URL};
