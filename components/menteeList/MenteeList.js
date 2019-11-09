962ee551e3cb6ae7bcd06a2ff9d9828c8f48352c
Matt Bongiovi <bongiovimatthew@gmail.com>
2019-11-09 13:13:50 +0000

    Remove MenteeListItem and use 'ListItem' instead

--- components/menteeList/MenteeList.js
+++ components/menteeList/MenteeList.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { View } from 'react-native';
-import MenteeListItem from './MenteeListItem';
-
+import { ListItem } from 'react-native-elements';
+import CONFIG from '../../config.js';
 
 const MenteeList = ({
   menteeItem,
   navigation
 }) => (
-  <View>
-    {menteeItem.map(item => (
-      <MenteeListItem navigation={navigation} key={item.id} item={item} />
-    ))}
-  </View>
-);
+    <View>
+      {menteeItem.map(item => (
+        <ListItem
+          title={item.name}
+          subtitle={item.school}
+          leftAvatar={{ source: { uri: `${CONFIG.FACEBOOK_PROFILE_LINK.PREFIX}${item.facebook_id}${CONFIG.FACEBOOK_PROFILE_LINK.SUFFIX}` } }}
+          bottomDivider
+          onPress={() => navigation.navigate('chat', { mentee: item.fullContact })}
+          key={item.id}
+        />
+      ))}
+    </View>
+  );
 
 export default MenteeList;
