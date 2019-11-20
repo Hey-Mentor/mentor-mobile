278a85f6e0c35e2bd27bc33266683de520f7ea95
Fredrik Forsell <fredrik.forsell@outlook.com>
2019-11-19 16:13:04 -0800

    Update twilioService.js
    + Added loop that checks for matching channels.
      - If it finds a match, open channel with channel.getchannel.
      - If not, send api call to create channel
    + subscribed to channelInvited event.
       - If user clicks ok -> joins channel

--- services/twilioService.js
+++ services/twilioService.js
@@ -1,12 +1,14 @@
 import { Client as TwilioChatClient } from 'twilio-chat';
-import { AsyncStorage } from 'react-native';
+import { AsyncStorage, Alert} from 'react-native';
 import CONFIG from '../config.js';
 import MessageService from './messageService.js';
+import { Paginator } from 'twilio-sync/lib/paginator';
 
 const API_URL = CONFIG.ENV === 'PROD' ? CONFIG.API_URL : CONFIG.TEST_API_URL;
 
 class TwilioService {
   constructor(hmToken, contacts, messagesCallback) {
+    
     this.hmToken = hmToken;
     this.id = `${JSON.parse(hmToken)._id}`;
 
@@ -71,39 +73,62 @@ class TwilioService {
     if (channel) {
       channel.on('messageAdded', message => this.messageService.updateLocalMessageStateSingle(contact, message));
       this.channels[contact] = channel;
-
-      // If we are the user who created the channel, always attempt to invite the other user
-      //  We only want to do this if we created the channel, because any other user on the channel
-      //   will not have permission to invite new users
-      if (channel.createdBy === this.id) {
-        this.tryInviteContact(contact);
-      }
-      return true;
     }
     return false;
   }
 
+
+
   async getChannelForChat(contact) {
     const channelName = this.getChannelName(contact);
-    return this.chatClient.getUserChannelDescriptors()
+    return this.chatClient.getPublicChannelDescriptors()
       .then((paginator) => {
         // If this user has channels already, check if there is a channel
         // between current user and the user being messaged
-        const channel = paginator.items.find(currentChannel => currentChannel.uniqueName === channelName);
-
-        // TODO: logging will prevent the "creation error" issue, but
-        //  we need a cleaner solution
-        // console.log(channel.channel);
-
-        if (channel && channel.channel) {
-          return channel.getChannel();
+        console.log('Amount of channels: ' + paginator.items.length);
+        for (i = 0; i < paginator.items.length; i++) {
+          const channel = paginator.items[i];
+          
+          if (channel.uniqueName === channelName) {
+            console.log("-            ---------------------- Channel already exist");
+            return channel.getChannel();
+          }
         }
-        return this.createChannelWithUser();
+
+        // const channel = paginator.items.find(currentChannel => {
+        //   return currentChannel.uniqueName === channelName;
+        // });
+
+        console.log('No channel found. Creating channel with user: ')
+        //return this.createChannelWithUser(contact);
+
+        //Creating channel through the api
+        return this.createChannelWithUserRequest(contact);
       });
+
+    return this.chatClient
+  }
+  async createChannelWithUserRequest(contact) {
+    const localToken = JSON.parse(this.hmToken);
+    const requestUri = `${API_URL}/chat/channel/create/${localToken._id}?token=${localToken.api_key}`;
+
+
+    // Adding the channelName to the body
+    const channelName = this.getChannelName(contact);
+    const bodyData = { channelName: channelName };
+    try {
+      const response = await fetch(requestUri, { method: 'post', body: JSON.stringify(bodyData), headers: { 'Content-Type': 'application/json' } });
+      const responseJson = await response.json();
+      const twilioToken = responseJson.status;
+      return twilioToken;
+    } catch (e) {
+      // TODO: Add sentry logging
+    }
   }
 
   async loadTwilioClient() {
     let localToken = await AsyncStorage.getItem('twilio_token');
+    console.log('twilio_token: ' + localToken);
 
     if (localToken) {
       const clientReady = await this.initChatClient(localToken);
@@ -144,6 +169,7 @@ class TwilioService {
 
   getChannelName(contact) {
     const localToken = JSON.parse(this.hmToken);
+    console.log(localToken._id > contact ? `${localToken._id}.${contact}` : `${contact}.${localToken._id}`);
     return localToken._id > contact ? `${localToken._id}.${contact}` : `${contact}.${localToken._id}`;
   }
 
@@ -185,6 +211,7 @@ class TwilioService {
     try {
       const chatClient = await TwilioChatClient.create(token, { logLevel: 'info' });
       this.chatClient = chatClient;
+      
 
       this.chatClient.on('tokenAboutToExpire', () => {
         this.getTwilioToken()
@@ -194,15 +221,10 @@ class TwilioService {
           });
       });
 
-      // Listen for new invitations to your Client
-      this.chatClient.on('channelInvited', (channel) => {
-        // Join the channel that you were invited to
-        // TODO: add a call to the backend to check if we should be joining this channel
-        channel.join();
-      });
+
 
       // TODO: Decide which of these callbacks we need
-      // this.subscribeToAllChatClientEvents();
+      this.subscribeToAllChatClientEvents();
       return true;
     } catch (e) {
       // TODO: add sentry logging
@@ -210,40 +232,69 @@ class TwilioService {
     }
   }
 
-  /*
+
   async subscribeToAllChatClientEvents() {
+
+    // Listen for new invitations to your Client
+    this.chatClient.on('channelInvited', (channel) => {
+      console.log('You have been invited to join the channel: ', channel.friendlyName)
+
+      // Works on both iOS and Android
+      Alert.alert(
+        'Channel invitation',
+        'You have been invited to join the channel: ' + channel.friendlyName,
+        [
+          { text: 'Ask me later', onPress: () => console.log('Ask me later pressed') },
+          {
+            text: 'Cancel',
+            onPress: () => console.log('Cancel Pressed'),
+            style: 'cancel',
+          },
+          { text: 'OK', onPress: () => {
+            console.log('OK Pressed') ;
+
+            // Join the channel that you were invited to
+            channel.join();
+          }},
+        ],
+        { cancelable: false },
+      );
+      // TODO: add a call to the backend to check if we should be joining this channel
+    });
+
+
     // TODO: Ensure that all webooks from this doc are included:
     // https://www.twilio.com/docs/chat/webhook-events
 
     // This function is not currently called. We should decide which callbacks we want to handle, and how
-    this.client.on('tokenAboutToExpire', obj => console.log('ChatClientHelper.client', 'tokenAboutToExpire', obj));
-    this.client.on('tokenExpired', obj => console.log('ChatClientHelper.client', 'tokenExpired', obj));
+    // this.client.on('tokenAboutToExpire', obj => console.log('ChatClientHelper.client', 'tokenAboutToExpire', obj));
+    // this.client.on('tokenExpired', obj => console.log('ChatClientHelper.client', 'tokenExpired', obj));
 
-    this.client.on('userSubscribed', obj => console.log('ChatClientHelper.client', 'userSubscribed', obj));
-    this.client.on('userUpdated', obj => console.log('ChatClientHelper.client', 'userUpdated', obj));
-    this.client.on('userUnsubscribed', obj => console.log('ChatClientHelper.client', 'userUnsubscribed', obj));
+    // this.client.on('userSubscribed', obj => console.log('ChatClientHelper.client', 'userSubscribed', obj));
+    // this.client.on('userUpdated', obj => console.log('ChatClientHelper.client', 'userUpdated', obj));
+    // this.client.on('userUnsubscribed', obj => console.log('ChatClientHelper.client', 'userUnsubscribed', obj));
 
-    this.client.on('channelAdded', obj => console.log('ChatClientHelper.client', 'channelAdded', obj));
-    this.client.on('channelRemoved', obj => console.log('ChatClientHelper.client', 'channelRemoved', obj));
-    this.client.on('channelInvited', obj => console.log('ChatClientHelper.client', 'channelInvited', obj));
-    this.client.on('channelJoined', obj => console.log('ChatClientHelper.client', 'channelJoined', obj));
-    this.client.on('channelLeft', obj => console.log('ChatClientHelper.client', 'channelLeft', obj));
-    this.client.on('channelUpdated', obj => console.log('ChatClientHelper.client', 'channelUpdated', obj));
+    // this.client.on('channelAdded', obj => console.log('ChatClientHelper.client', 'channelAdded', obj));
+    // this.client.on('channelRemoved', obj => console.log('ChatClientHelper.client', 'channelRemoved', obj));
+    // this.client.on('channelInvited', obj => console.log('ChatClientHelper.client', 'channelInvited', obj));
+    // this.client.on('channelJoined', obj => console.log('ChatClientHelper.client', 'channelJoined', obj));
+    // this.client.on('channelLeft', obj => console.log('ChatClientHelper.client', 'channelLeft', obj));
+    // this.client.on('channelUpdated', obj => console.log('ChatClientHelper.client', 'channelUpdated', obj));
 
-    this.client.on('memberJoined', obj => console.log('ChatClientHelper.client', 'memberJoined', obj));
-    this.client.on('memberLeft', obj => console.log('ChatClientHelper.client', 'memberLeft', obj));
-    this.client.on('memberUpdated', obj => console.log('ChatClientHelper.client', 'memberUpdated', obj));
+    // this.client.on('memberJoined', obj => console.log('ChatClientHelper.client', 'memberJoined', obj));
+    // this.client.on('memberLeft', obj => console.log('ChatClientHelper.client', 'memberLeft', obj));
+    // this.client.on('memberUpdated', obj => console.log('ChatClientHelper.client', 'memberUpdated', obj));
 
-    this.client.on('messageAdded', obj => console.log('ChatClientHelper.client', 'messageAdded', obj));
-    this.client.on('messageUpdated', obj => console.log('ChatClientHelper.client', 'messageUpdated', obj));
-    this.client.on('messageRemoved', obj => console.log('ChatClientHelper.client', 'messageRemoved', obj));
+    // this.client.on('messageAdded', obj => console.log('ChatClientHelper.client', 'messageAdded', obj));
+    // this.client.on('messageUpdated', obj => console.log('ChatClientHelper.client', 'messageUpdated', obj));
+    // this.client.on('messageRemoved', obj => console.log('ChatClientHelper.client', 'messageRemoved', obj));
 
-    this.client.on('typingStarted', obj => console.log('ChatClientHelper.client', 'typingStarted', obj));
-    this.client.on('typingEnded', obj => console.log('ChatClientHelper.client', 'typingEnded', obj));
+    // this.client.on('typingStarted', obj => console.log('ChatClientHelper.client', 'typingStarted', obj));
+    // this.client.on('typingEnded', obj => console.log('ChatClientHelper.client', 'typingEnded', obj));
 
-    this.client.on('connectionStateChanged', obj => console.log('ChatClientHelper.client', 'connectionStateChanged', obj));
-    this.client.on('pushNotification', obj => console.log('ChatClientHelper.client', 'onPushNotification', obj));
-  } */
+    // this.client.on('connectionStateChanged', obj => console.log('ChatClientHelper.client', 'connectionStateChanged', obj));
+    // this.client.on('pushNotification', obj => console.log('ChatClientHelper.client', 'onPushNotification', obj));
+  }
 }
 
 export default TwilioService;
